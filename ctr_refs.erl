08a80772210cc596be935f8543395e706454a98e
Nicolas Favre-Felix <n.favrefelix@gmail.com>
2013-05-31 17:40:17 +0100

    Start to improve ctr_refs

--- ctr_refs.erl
+++ ctr_refs.erl
@@ -5,39 +5,51 @@
 -define(GC_TIME_THRESHOLD, 1000000). % 1 sec
 
 % This module is used by non-idempotent counters
-
-dedupe(Increments) ->
-    lists:ukeysort(1, Increments).
+keep_left(_K, L, _R) -> L.
 
 new(Value) -> % single increment w/ ID & time
 	new(Value, make_ref()).
 
 new(Value, UniqId) -> % single increment w/ ID & time
-    [{UniqId, {now(), Value}}].
+	orddict:store(UniqId, {now(), Value}, orddict:new()).
 
 merge(L,R) ->
-    dedupe(lists:keymerge(1, L, R)). % merge increments
+	% keep left value in case of key conflict (this is arbitrary)
+	orddict:merge(fun keep_left/3, L, R).
 
 value(C, Fun) ->
-    Deduped = dedupe(C),
-    Fun([Delta || {_Id,{_Timestamp,Delta}} <- Deduped]).
+	% TODO: use orddict:fold instead
+    Fun([Delta || {_Id,{_Timestamp,Delta}} <- orddict:to_list(C)]).
 
 gc_info(C) -> % extract all "non-recent" increments
-    Increments = lists:sublist(C, ?MAX_GC_ITEMS), % first N items
+	L = orddict:to_list(C),
+    Increments = lists:sublist(L, ?MAX_GC_ITEMS), % first N items
     Now = erlang:now(),
     OldIncrements = lists:filter( % filter out increments that are too old
         fun({_Id,{Timestamp,_Delta}}) ->
                 timer:now_diff(Now, Timestamp) > ?GC_TIME_THRESHOLD
         end, Increments),
-    OldIncrements.
+    orddict:from_list(OldIncrements).
 
 gc_merge(CounterModule, C, GcData, UniqId) ->
-    AllIncrements = lists:concat(GcData),
-    ToAdd = new(CounterModule:value(AllIncrements), UniqId),
-    RefsToRemove = sets:from_list([Ref || {Ref,_} <- AllIncrements]),
+
+	% merge all GC info in one dict
+	AllIncrements = lists:foldr(
+		fun (OL, OR) ->
+			orddict:merge(fun keep_left/3, OL, OR)
+		end,
+		orddict:new(), GcData),
+
+	% remove increments
+    RefsToRemove = orddict:fetch_keys(AllIncrements),
     Cleaned = lists:foldr(fun(Ref, Cur) -> 
-            lists:keydelete(Ref, 1, Cur) % Remove "Ref" from C
-        end, C, sets:to_list(RefsToRemove)), % for all refs in "ToRemove"
+            orddict:erase(Ref, Cur) % Remove "Ref" from C
+        end, C, RefsToRemove), % for all refs in "ToRemove"
+
+	io:format("Cleaned=~w~n", [Cleaned]),
+
+    % AllIncrements = lists:concat(GcData),
+    ToAdd = new(CounterModule:value(AllIncrements), UniqId),
 
     Ret = merge(ToAdd, Cleaned), % Add the new counter to "Cleaned"
     Ret.
